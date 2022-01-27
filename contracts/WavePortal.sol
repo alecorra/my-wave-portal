d6d09f62a27d94098777c4c4ccc3a524d2eb5ef3
Alessandro Corra <alessandro.corra@2132.io>
2022-01-27 23:29:05 +0000

    store data

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,24 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract WavePortal {
+  uint256 totalWaves;
+
   constructor() {
     console.log("I'm the smart contract!");
+  }
+
+  // public here means that the function become available to be called on the blockchain;
+  // this is like a public API endpoint!
+  function wave() public {
+    totalWaves += 1;
+
+    // msg.sender is the wallet address of the person who called the function.
+    // it's like a built-in authentication
+    console.log("%s has waved!", msg.sender);
+  }
+
+  function getTotalWaves() public view returns (uint256) {
+    console.log("We have %d total waves!", totalWaves);
+    return totalWaves;
   }
 }
